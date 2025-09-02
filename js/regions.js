947357f155428fe3c761a0c0a7d1e3730b92a535
pennelldj <dpennell@stiwdio.com>
2025-09-02 20:12:41 +0100

    Regiosn added

--- js/regions.js
+++ js/regions.js
@@ -19,7 +19,7 @@ const REGIONS = {
     'somerset','lincolnshire','hampshire','isle of wight','durham','tyne',
     'hartlepool','whitby','scarborough','redcar','blackpool','south shields',
     'jersey','guernsey','alderney','isle of man',
-    'suffolk','hants','southend-on-sea'   
+    'suffolk','hants','southend-on-sea','mudeford', 'wicklow'   
   ],
   ireland: [
     'dublin','galway','cork','belfast','londonderry','waterford',
